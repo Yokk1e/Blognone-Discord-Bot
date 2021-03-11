9ba7e021253f3377e72d3cc73d7f9233d24f6910
Yok <satthabut.o@gmail.com>
2021-03-11 05:48:35 +0000

    - [feat] add update blog to db

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import requests
 from bs4 import BeautifulSoup
+from replit import db
 
 client = discord.Client()
 
@@ -17,6 +18,14 @@ def get_new_blognnone_contetnt():
     tag_a = title_box.find('a')
     print(tag_a['href'])
 
+def update_blogs(blog_path):
+  if "blog_path" in db.keys():
+    blogs_path = db["blog_path"]
+    blogs_path.append(blog_path)
+    db["blog_path"] = blogs_path
+  else:
+    db["blog_path"] = [blog_path]
+    
 @client.event
 async def on_ready():
   print('blognone content ready')
