c8e7c38f6e9827b36abb63f7dac91c000d7a238e
Yok <satthabut.o@gmail.com>
2021-03-11 05:51:43 +0000

    - [style] change name

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ client = discord.Client()
 def get_new_blognnone_contetnt():
   URL = 'https://www.blognone.com'
   page = requests.get(URL)
-  soup = BeautifulSoup(page.content, 'html.parser')
-  result_main = soup.find('div',id='block-system-main')
-  contents = result_main.find('div',class_='content')
+  blog_contents_html = BeautifulSoup(page.content, 'html.parser')
+  blogs = blog_contents_html.find('div',id='block-system-main')
+  contents = blogs.find('div',class_='content')
   contents_title_box = contents.find_all('div',class_='content-title-box')
 
   for title_box in contents_title_box:
     tag_a = title_box.find('a')
-    print(tag_a['href'])
+    print(URL+tag_a['href'])
 
 def update_blogs(blog_path):
   if "blog_path" in db.keys():
@@ -25,7 +25,7 @@ def update_blogs(blog_path):
     db["blog_path"] = blogs_path
   else:
     db["blog_path"] = [blog_path]
-    
+
 @client.event
 async def on_ready():
   print('blognone content ready')
