27f2a37cb0807464c3605a976d9bd42a6e65b858
Yok <satthabut.o@gmail.com>
2021-03-11 09:01:02 +0000

    - [feat] update condition check bot

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
+  if message.author.bot:
     return
 
   new_blogs = get_new_blognnone_contetnt()
