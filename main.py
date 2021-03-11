9b741153c38e4d019b6cb36a4fe50fab249ab1fb
Yok <satthabut.o@gmail.com>
2021-03-11 05:19:35 +0000

    - [feat] update get path blognone content

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import discord
 import os
+import requests
+from bs4 import BeautifulSoup
 
 client = discord.Client()
 
+def get_new_blognnone_contetnt():
+  URL = 'https://www.blognone.com'
+  page = requests.get(URL)
+  soup = BeautifulSoup(page.content, 'html.parser')
+  result_main = soup.find('div',id='block-system-main')
+  contents = result_main.find('div',class_='content')
+  contents_title_box = contents.find_all('div',class_='content-title-box')
+
+  for title_box in contents_title_box:
+    tag_a = title_box.find('a')
+    print(tag_a['href'])
+
 @client.event
 async def on_ready():
-  print('Hi YOkk1e')
+  print('blognone content ready')
 
 @client.event
 async def on_message(message):
   if message.author == client:
     return
+
+  get_new_blognnone_contetnt()
   
-  if message.content.startswith('$test'):
-    await message.channel.send('test naja')
+
+  # print(content.prettify())
+  # if message.content.startswith('$getHTML'):
+    # await message.channel.send(content.prettify())
 
 client.run(os.getenv('TOKEN'))
